48c47da3f954d158606ba103b676a1f8074749f5
dusan-d <113931441+dusan-d@users.noreply.github.com>
2024-01-29 16:10:07 +0100

    Added label, button, image view and text field

--- Module3/Module3/ViewControllers/CustomViewController.swift
+++ Module3/Module3/ViewControllers/CustomViewController.swift
@@ -10,10 +10,19 @@ import UIKit
 
 class CustomViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var button: UIButton!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var textField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        setupLabel()
+        setupButton()
+        setupImageView()
+        setupTextField()
     }
     
 
@@ -26,4 +35,45 @@ class CustomViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+
+    @IBAction func buttonTapped(_ sender: Any) {
+        print("Button Tapped")
+    }
+    
+    private func setupLabel() {
+        label.text = "Hello, students!"
+        label.font = UIFont.systemFont(ofSize: 14)
+        label.textAlignment = .center
+        label.textColor = .red
+        label.numberOfLines = 0
+    }
+
+    private func setupButton() {
+        button.setTitle("PRESS", for: .normal) // .disabled, .selected etc.
+        button.setTitleColor(.red, for: .normal)
+        button.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
+    }
+
+    private func setupImageView() {
+        imageView.image = UIImage(named: "background")
+        imageView.backgroundColor = .clear
+    }
+
+    private func setupTextField() {
+        textField.delegate = self
+    }
+}
+
+extension CustomViewController: UITextFieldDelegate {
+    // Tells the delegate when the text selection changes in the specified text field.
+    func textFieldDidChangeSelection(_ textField: UITextField) {
+        print("Text changed: \(textField.text ?? "")")
+    }
+
+    // Asks the delegate whether to process the pressing of the Return button for the text field.
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        // Dismiss the keyboard
+        textField.resignFirstResponder()
+        return true
+    }
 }
